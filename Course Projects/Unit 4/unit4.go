63ef193ea40b97a409b8e6b28685e461b874f87c
InvertedTNT <tzbodula@gmail.com>
2020-05-27 14:29:27 -0400

    Global Scope Variables

--- Course Projects/Unit 4/unit4.go
+++ Course Projects/Unit 4/unit4.go
@@ -4,15 +4,19 @@ import (
 	"fmt"
 )
 
+var a = 5 //Declares a global variable named A and assigns it to the value 5. (Initialization)
+var b int //Declares that there is a global variable with the name Z and that the variable is of type int. Assigns the "zero value" of the integer type to "z"
+
+//The "zero value" is essentially a default value. For instance, the zero value for numbers is 0 and the zero value for booleans is false.
+
 func main() {
 	n, _ := fmt.Println("Hello, playground", 42, true)
 	fmt.Println(n)
-
 	x := 42 //Declares and assigns the value 42 to the variable X
 	fmt.Println(x)
 	x = 99 //Gives the variable X a new value of 99
 
-	y := x + 1 //Declares and assigns the value of X plus one and assigns it to the variable Y
+	y := x + a //Declares and assigns the value of X plus one and assigns it to the variable Y
 
 	z := x * y           //Declares and multiplies the value of x and y and assigns it to the variable Z
 	fmt.Println(x, y, z) //Prints x, y and z.
